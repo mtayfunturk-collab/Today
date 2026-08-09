d4cbca5b035b9652089200c03d12011c575f49d8
mtayfunturk-collab <mtayfunturk@icloud.com>
2026-08-09 21:32:31 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
  * Uygulama kabuğunda (özellikle index.html) değişiklik yapıldığında
  * bu sürüm mutlaka artırılmalıdır.
  */
-const VERSION = "today-v2-foundation-051";
+const VERSION = "today-v2-foundation-052";
 const CACHE_NAME = `today-cache-${VERSION}`;
 const ACTIVATE_MESSAGE =
   "TODAY_ACTIVATE_UPDATE";
